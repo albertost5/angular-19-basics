fa1a9c54b73e568c64d4a86d5ae9423f14fef68d
albertost5 <sancheztorresalberto@gmail.com>
2025-05-14 19:54:13 +0200

    Implemented form and added validation and error cases

--- src/app/pages/reactive-forms/reactive/basic-page/basic-page.component.ts
+++ src/app/pages/reactive-forms/reactive/basic-page/basic-page.component.ts
@@ -1,15 +1,54 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {JsonPipe} from '@angular/common';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
   imports: [
-    JsonPipe
+    JsonPipe,
+    ReactiveFormsModule
   ],
   templateUrl: './basic-page.component.html',
   standalone: true,
   styles: ``
 })
 export class BasicPageComponent {
+  // myForm = new FormGroup({
+  //   product: new FormControl(''),
+  //   price: new FormControl(0),
+  //   stock: new FormControl(0),
+  // });
 
+  private fb: FormBuilder = inject(FormBuilder);
+  myForm = this.fb.group({
+    product: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+  })
+
+  isInvalidField(fieldName: string): boolean {
+    return !!this.myForm.get(fieldName)?.errors && !!this.myForm.get(fieldName)?.touched;
+  }
+
+  getFieldError(fieldName: string): string {
+    const formControlErrors = this.myForm.get(fieldName)?.errors;
+    switch (Object.keys(formControlErrors!)[0]) {
+      case 'required':
+        return `The field ${fieldName} is required`;
+      case 'minlength':
+        return `The min length is ${formControlErrors!['minlength']['requiredLength']}`;
+      case 'min':
+        return `The min value is ${formControlErrors!['min']['min']}`
+    }
+    return '';
+  }
+
+  onSubmit() {
+    if (!this.myForm.valid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 }
